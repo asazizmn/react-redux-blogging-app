4c4aae729f31130dd761e919a0cc967364e65572
S. Aziz <azizecomm@gmail.com>
2020-11-03 12:46:05 +0800

    add comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+/**
+ * index.js
+ * - react and redux starting point
+ */
+
+
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+
+
+/*
+ * Main React Component
+ */
 class App extends React.Component {
   render() {
     return (
@@ -9,6 +20,8 @@ class App extends React.Component {
   }
 }
 
+
+// render main component and insert code in html
 ReactDOM.render((
   <App />
 ), document.getElementById('root'));
